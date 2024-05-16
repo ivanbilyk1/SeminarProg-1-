2ead2ec46108eb7b4abfab2e5b315b83ba5b37e1
Ivan Bilyk <thinkingyyy@gmail.com>
2024-05-16 23:40:51 +0300

    .

--- SeminarProg(1)/Source.cpp
+++ SeminarProg(1)/Source.cpp
@@ -4,9 +4,15 @@ void Endiannes() {
 	unsigned short int n = 0x1234;
 	unsigned char* p = (unsigned char*)&n;
 
-
+	if (p[0] == 0x12 && p[1] == 0x34) {
+		printf("Big-endian\n");
+	}
+	else if (p[0] == 0x34 && p[1] == 0x12) {
+		printf("Little-endian\n");
+	}
 }
 
 int main() {
+	Endiannes();
 	return 0;
 }
